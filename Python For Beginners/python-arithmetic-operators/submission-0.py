91f91ad9a7577e2141db7d7ec66a9e64c696467b
Tyler B <113268013+Tyler-Beck@users.noreply.github.com>
2026-05-24 08:53:10 -0400

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,10 @@
+a, b, c = 2, 2, 0.5
+sum_vals = (a + b + c)
+minus_0 = 0 - (a + b + c)
+product = (a * b * c)
+mix = (sum_vals / product)
+
+print(sum_vals)
+print(minus_0)
+print(product)
+print(mix)
